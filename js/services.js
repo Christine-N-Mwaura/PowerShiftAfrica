2ba9f9323b09f5babc8d08618dcd8f47d4d4a043
Christine-N-Mwaura <njerimwaura25@gmail.com>
2021-10-28 07:06:34 +0300

    added a skeleton of the what we do pages

--- js/services.js
+++ js/services.js
@@ -41,6 +41,8 @@ $(document).ready(function()
 
 	initMenu();
 	initSvg();
+	new WOW().init();
+	initSlideProjects()
 
 	/* 
 
@@ -152,6 +154,33 @@ $(document).ready(function()
 				// Replace image with new SVG
 				$img.replaceWith($svg);
 			}, 'xml');
+		});
+	}
+	function initSlideProjects()
+	{
+		$('#carousel-example').on('slide.bs.carousel', function (e) {
+
+			/*
+				CC 2.0 License Iatek LLC 2018
+				Attribution required
+			*/
+			var $e = $(e.relatedTarget);
+			var idx = $e.index();
+			var itemsPerSlide = 5;
+			var totalItems = $('.carousel-item').length;
+			
+			if (idx >= totalItems-(itemsPerSlide-1)) {
+				var it = itemsPerSlide - (totalItems - idx);
+				for (var i=0; i<it; i++) {
+					// append slides to end
+					if (e.direction=="left") {
+						$('.carousel-item').eq(i).appendTo('.carousel-inner');
+					}
+					else {
+						$('.carousel-item').eq(0).appendTo('.carousel-inner');
+					}
+				}
+			}
 		});
 	}
 
